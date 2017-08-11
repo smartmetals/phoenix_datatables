f7edf99047509390527130b63e1c866de0fd6d51
Jeremy Huffman <jeremy@jeremyhuffman.com>
2017-08-11 13:40:16 -0400

    HTML data tables working.

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,6 +12,7 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+import init_table from "./table"
 
 // Import local files
 //
@@ -19,3 +20,5 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+init_table();
