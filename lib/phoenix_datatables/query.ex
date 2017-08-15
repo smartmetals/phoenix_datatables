3f9d0763adf85c72ff1dfa02949e15266e0266d4
Jeremy Huffman <jeremy@jeremyhuffman.com>
2017-08-15 17:20:17 -0400

    Most basic possible sorting working.

--- lib/phoenix_datatables/query.ex
+++ lib/phoenix_datatables/query.ex
@@ -1,3 +1,25 @@
 defmodule PhoenixDatatables.Query do
+  import Ecto.Query
+  alias PhoenixDatatables.Request.Params
+
+  def sort(%Params{order: order} = params, queryable, sortable) do
+    [order] = order
+    dir = cast_dir(order.dir)
+    column = params.columns[order.column].data |> cast_column(sortable)
+    queryable
+    |> order_by([{^dir, ^column}])
+  end
+
+  defp cast_column(column_name, sortable) do
+    if column_name in Enum.map(sortable, &Atom.to_string/1) do
+      String.to_atom(column_name)
+    else
+      raise ArgumentError, "#{column_name} is not a sortable column."
+    end
+  end
+
+  defp cast_dir("asc"), do: :asc
+  defp cast_dir("desc"), do: :desc
+  defp cast_dir(wrong), do: raise ArgumentError, "#{wrong} is not a valid sort order."
 
 end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -10,4 +10,25 @@ defmodule PhoenixDatatablesExample.Factory do
       aac: "H"
     }
   end
+
+  def raw_request do
+    %{
+      "_" => "1502482464715",
+      "columns" =>
+        %{
+          "0" => %{"data" => "nsn", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"},
+          "1" => %{"data" => "rep_office", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"},
+          "2" => %{"data" => "common_name", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"},
+          "3" => %{"data" => "description", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"},
+          "4" => %{"data" => "price", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"},
+          "5" => %{"data" => "ui", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"},
+          "6" => %{"data" => "aac", "name" => "", "orderable" => "true", "search" => %{"regex" => "false", "value" => ""}, "searchable" => "true"}
+        },
+      "draw" => "1",
+      "length" => "10",
+      "order" => %{"0" => %{"column" => "0", "dir" => "asc"}},
+      "search" => %{"regex" => "false", "value" => ""},
+      "start" => "0"
+    }
+  end
 end
