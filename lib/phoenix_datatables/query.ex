0f0741807c5d09ac27aaf0d8b1e1aa8fba896268
Arun Karottu <arunkarottu@gmail.com>
2017-08-15 16:41:41 -0400

    Add pagination.

--- lib/phoenix_datatables/query.ex
+++ lib/phoenix_datatables/query.ex
@@ -22,4 +22,35 @@ defmodule PhoenixDatatables.Query do
   defp cast_dir("desc"), do: :desc
   defp cast_dir(wrong), do: raise ArgumentError, "#{wrong} is not a valid sort order."
 
+  def paginate(queryable, params) do
+    length = convert_to_number_if_string(params.length)
+    start = convert_to_number_if_string(params.start)
+
+    queryable
+    |> limit(^length)
+    |> offset(^start)
+  end
+
+  # credit to the scrivener github: https://github.com/drewolson/scrivener_ecto/blob/master/lib/scrivener/paginater/ecto/query.ex
+  def total_entries(queryable, repo) do
+    total_entries =
+      queryable
+      |> exclude(:preload)
+      |> exclude(:select)
+      |> exclude(:order_by)
+      |> subquery
+      |> select(count("*"))
+      |> repo.one
+
+    total_entries || 0
+  end
+
+  defp convert_to_number_if_string(num) do
+    case is_binary(num) do
+      true ->
+        {num, _} = Integer.parse(num)
+        num
+      false -> num
+    end
+  end
 end
