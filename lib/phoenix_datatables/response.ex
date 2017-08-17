3749f9f3d1e7bf35201cc94bbe1eb2bc807872f8
Arun Karottu <arunkarottu@gmail.com>
2017-08-17 14:10:56 -0400

    Response - Payload and Send (#6)
    * Payload module follows the format in which datatables expects a response
     * Send accepts query, draw, schema module and Repo and returns all the required fields for datatables
    * Queries are run within send (after being composed in sort, search etc.).

--- lib/phoenix_datatables/response.ex
+++ lib/phoenix_datatables/response.ex
@@ -1,3 +1,33 @@
-defmodule PhoenixDatatables.Response do
+defmodule PhoenixDatatables.Response.Payload do
   defstruct draw: 0, recordsTotal: 0, recordsFiltered: 0, data: [%{}], error: nil
+end
+
+defmodule PhoenixDatatables.Response do
+  import Ecto.Query
+
+  alias PhoenixDatatables.Response.Payload
+
+  def send(query, draw, total_entries, repo) do
+    %Payload {
+      draw: draw,
+      recordsTotal: total_entries,
+      recordsFiltered: total_entries(query, repo),
+      data: repo.all(query),
+      error: nil
+    }
+  end
+
+  # credit to scrivener library: https://github.com/drewolson/scrivener_ecto/blob/master/lib/scrivener/paginater/ecto/query.ex
+  def total_entries(queryable, repo) do
+    total_entries =
+      queryable
+      |> exclude(:preload)
+      |> exclude(:select)
+      |> exclude(:order_by)
+      |> subquery
+      |> select(count("*"))
+      |> repo.one
+
+    total_entries || 0
+  end
 end
---
+++ test/phoenix_datatables/response_test.ex
@@ -0,0 +1,71 @@
+defmodule PhoenixDatatables.ResponseTest do
+  use PhoenixDatatablesExample.DataCase
+  alias PhoenixDatatables.Response
+  alias PhoenixDatatables.Request
+  alias PhoenixDatatables.Query
+  alias PhoenixDatatablesExample.Repo
+  alias PhoenixDatatablesExample.Stock.Item
+  alias PhoenixDatatablesExample.Stock.Category
+  alias PhoenixDatatablesExample.Factory
+
+  describe "send" do
+    test "returns queried data in correct format" do
+      add_items()
+      query =
+        (from item in Item,
+          join: category in assoc(item, :category),
+          select: %{id: item.id, category_name: category.name, nsn: item.nsn})
+      request =
+        Map.put(
+          Factory.raw_request,
+          "search",
+          %{"regex" => "false", "value" => "1NSN"}
+        )
+        |> Request.receive
+      search_results = Query.search(request, query)
+      payload = Response.send(search_results, request.draw, Response.total_entries(Item, Repo), Repo)
+
+      assert payload.draw == request.draw
+      assert payload.recordsFiltered == length(Repo.all(search_results))
+      assert payload.recordsTotal == length(Repo.all(Item))
+      assert payload.data == Repo.all(search_results)
+    end
+  end
+
+  describe "total_entries" do
+    test "returns number of results in specified schema" do
+      add_items()
+      assert Response.total_entries(Item, Repo) == length(Repo.all(Item))
+    end
+
+    test "returns number of results in a query" do
+      add_items()
+      query =
+        (from item in Item,
+          join: category in assoc(item, :category),
+          select: %{id: item.id})
+      assert Response.total_entries(query, Repo) == length(Repo.all(Item))
+    end
+  end
+
+  def add_items do
+    category_a = insert_category!("A")
+    category_b = insert_category!("B")
+    item = Map.put(Factory.item, :category_id, category_b.id)
+    item2 = Map.put(Factory.item, :category_id, category_a.id)
+    item2 = %{item2 | nsn: "1NSN"}
+    one = insert_item! item
+    two = insert_item! item2
+    [one, two]
+  end
+
+  def insert_item!(item) do
+    cs = Item.changeset(%Item{}, item)
+    Repo.insert!(cs)
+  end
+
+  def insert_category!(category) do
+    cs = Category.changeset(%Category{}, %{name: category})
+    Repo.insert!(cs)
+  end
+end
