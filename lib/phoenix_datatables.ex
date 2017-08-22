41c7f0857ab77c2de75fe2ece37f8717552d1170
Jeremy Huffman <jeremy@jeremyhuffman.com>
2017-08-22 17:38:02 -0400

    WIP transfer

--- lib/phoenix_datatables.ex
+++ lib/phoenix_datatables.ex
@@ -2,6 +2,7 @@ defmodule PhoenixDatatables do
   alias PhoenixDatatables.Request
   alias PhoenixDatatables.Query
   alias PhoenixDatatables.Response
+  alias PhoenixDatatables.Response.Payload
 
   def execute(query, params, repo), do: do_execute(query, params, repo, nil)
   def execute(query, params, repo, columns), do: do_execute(query, params, repo, columns)
@@ -13,5 +14,11 @@ defmodule PhoenixDatatables do
     |> Query.search(params, columns)
     |> Query.paginate(params)
     |> Response.send(params.draw, Response.total_entries(query, repo), repo)
+  end
+
+  def map_payload(%Payload{} = payload, fun) when is_function(fun) do
+    %Payload { payload |
+      data: Enum.map(payload.data, fun)
+    }
   end
 end
