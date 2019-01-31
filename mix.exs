cdf4688ce4b397f8032177045567ccd8058fb09a
Jeremy Huffman <jeremy@jeremyhuffman.com>
2019-01-31 08:38:16 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhoenixDatatables.Mixfile do
   def project do
     [
       app: :phoenix_datatables,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env),
       deps: deps(),
